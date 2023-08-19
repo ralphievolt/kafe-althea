b01650a9dd801708364e73228e247d6a5025dfb3
ralphie <ralpheiligan@gmail.com>
2023-08-19 07:02:38 -0400

    test repo renamed push

--- app/coffee/page.tsx
+++ app/coffee/page.tsx
@@ -1,9 +1,13 @@
 import { title } from "@/components/primitives";
+import { CoffeeMenu } from "@/components/menu/coffee";
 
 export default function CoffeePage() {
   return (
     <div>
       <h1 className={title()}>Coffee</h1>
+      <p>
+        <CoffeeMenu />
+      </p>
     </div>
   );
 }
